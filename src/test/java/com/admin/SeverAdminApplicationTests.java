3f5c977f9d73a5a07c5d4c7296279e15c4f42635
prakash052000 <prakashmaniraj07@gmail.com>
2025-03-27 23:26:55 +0530

    Initial commit

---
+++ src/test/java/com/admin/SeverAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
